b343c944a162d3973271281c0146e180cc7d1fb1
Booze33 <tebetisloh@gmail.com>
2024-02-07 16:26:34 +0100

    Made some changes on project section

--- src/components/projects/recipe.jsx
+++ src/components/projects/recipe.jsx
@@ -1,12 +1,33 @@
 import React, { useRef } from 'react';
 import { motion } from 'framer-motion';
 
+const pathVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 3,
+      delay: 1,
+      ease: "easeInOut"
+    }
+  }
+};
+
 const RecipeApp = () => {
     const scrollRef = useRef(null);
 
   return (
     <div className="w-screen h-full mt-4 pt-12 px-8 sm:px-16" ref={scrollRef}>
-      <h1>RecipeApp</h1>
+      <div>
+        <motion.h1
+          variants={pathVariants}
+          initial="hidden"
+          whileInView="visible"
+          className="text-5xl sm:text-7xl text-center sm:w-display w-feature font-curved"
+        >RecipeApp</motion.h1>
+      </div>
     </div>
   )
 }
--- src/components/projects/rover.jsx
+++ src/components/projects/rover.jsx
@@ -1,12 +1,33 @@
 import React, { useRef } from 'react';
 import { motion } from 'framer-motion';
 
+const pathVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 3,
+      delay: 1,
+      ease: "easeInOut"
+    }
+  }
+};
+
 const RoverApp = () => {
   const scrollRef = useRef(null);
 
   return (
     <div className="w-screen h-full mt-4 pt-12 px-8 sm:px-16" ref={scrollRef}>
-      <h1>RoverApp store app</h1>
+      <div>
+        <motion.h1
+          variants={pathVariants}
+          initial="hidden"
+          whileInView="visible"
+          className="text-5xl sm:text-7xl text-center sm:w-display w-feature font-curved"
+        >RoverApp store app</motion.h1>
+      </div>
     </div>
   )
 }
