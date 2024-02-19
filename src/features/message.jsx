ed2dbcb84e569080fa4d94d2bccc90d6e52c52dc
Booze33 <tebetisloh@gmail.com>
2024-02-20 00:17:16 +0100

    Made some changes on project section

--- src/features/message.jsx
+++ src/features/message.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import '../assets/stylesheet/message.css';
 
 const Message = () => {
@@ -7,6 +8,25 @@ const Message = () => {
   const handleClick = () => {
     setIsActive(!isActive);
   }
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_4beqc1i', 'template_krgk2be', form.current, {
+        publicKey: 'd_c1ttLLXIDyzHaIO',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
 
   return (
     <>
@@ -19,7 +39,15 @@ const Message = () => {
         />
       </button>
       <div className={`mobile-nav ${isActive ? 'is-active' : ''}`}>
-        <h1>Tisloh</h1>
+      <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
       </div>
     </>
   )
