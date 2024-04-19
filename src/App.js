162d5e5c0e2e69b68fc712c035b5b5d75dcc77f4
Booze33 <tebetisloh@gmail.com>
2024-04-19 12:18:27 +0200

    update portfolio

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import RoverApp from './components/projects/rover';
 import ExpenseTracker from './components/projects/expense';
 import RecipeApp from './components/projects/recipe';
 import Book from './components/projects/book';
+import Web3 from './components/projects/web3';
+import AiChat from './components/projects/AiChat';
 import { AnimatePresence } from 'framer-motion';
 
 const App = () => {
@@ -21,6 +23,8 @@ const App = () => {
             <Route path="/projects/rover-app" element={<RoverApp />} />
             <Route path="/projects/book-store" element={<Book />} />
             <Route path="/projects/recipe-app" element={<RecipeApp />} />
+            <Route path="/projects/ai-Chat" element={<AiChat />} />
+            <Route path="/projects/web3-Crowd" element={<Web3 />} />
             <Route path="/projects/expense-tracker" element={<ExpenseTracker />} />
           </Routes>
           <Message />
