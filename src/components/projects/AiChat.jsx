e33e2ac80341baa17f17841e1d1589fb9f914fa8
Booze33 <tebetisloh@gmail.com>
2024-04-26 10:38:14 +0200

    update portfolio

--- src/components/projects/AiChat.jsx
+++ src/components/projects/AiChat.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { motion } from 'framer-motion';
+import { FaGithub } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import { HiArrowUturnLeft } from "react-icons/hi2";
+import image from '../../assets/images/chat.png';
+import commerce from '../../assets/images/chat(2).png';
+import Footer from '../footer';
+
+const pathVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 3,
+      delay: 1,
+      ease: "easeInOut"
+    }
+  }
+};
 
 const AiChat = () => {
+  const scrollRef = useRef(null);
   return (
     <div>
       
